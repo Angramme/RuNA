97b1d841b1ebabad3536cfc650cf951b5e2c9de2
Angramme <kacpero376@gmail.com>
2022-11-29 20:50:18 +0100

    upgraded test file

--- tests/mem_use.rs
+++ tests/mem_use.rs
@@ -1,12 +1,64 @@
 use std::env;
 use runa::{dna::{DnaMetricSpace as Dms}, math::*, io::read_test_inst};
 
-fn main(){
-    if env::args().nth(1) != Some("mem_use".to_string()) {
-        println!("an argument saying \"mem_use\" is required");
-        return;
-    }
-    let block = read_test_inst("Inst_0000013_45.adn").expect("cannot read block!");
 
+fn main_dist_naif(){
+    let block = read_test_inst("Inst_0000013_45.adn").expect("cannot read block!");
+    let res = dist_naif::<Dms>(&block.0, &block.1);
+    println!("{}", res);
+}
+
+fn main_dist_1(){
+    let block = read_test_inst("Inst_0010000_7.adn").expect("cannot read block!");
+    let res = dist_1::<Dms>(&block.0, &block.1);
+    println!("{}", res);
+}
+
+fn main_sol_1(){
+    let block = read_test_inst("Inst_0010000_7.adn").expect("cannot read block!");
+    let res = sol_1::<Dms>(&block.0, &block.1);
+    println!("{}", res);
+}
+
+fn main_prog_dyn(){
+    let block = read_test_inst("Inst_0010000_7.adn").expect("cannot read block!");
+    let res = prog_dyn::<Dms>(&block.0, &block.1);
+    println!("{}", res.0);
+}
+
+fn main_dist_2(){
+    let block = read_test_inst("Inst_0010000_7.adn").expect("cannot read block!");
     dist_naif::<Dms>(&block.0, &block.1);
+}
+
+fn main_sol_2(){
+    // TODO
+    // let block = read_test_inst("Inst_0000013_45.adn").expect("cannot read block!");
+    // dist_naif::<Dms>(&block.0, &block.1);
+}
+
+
+fn main(){
+    let funcs = [
+        ("dist_naif", main_dist_naif as fn()),
+        ("dist_1", main_dist_1),
+        ("dist_2", main_dist_2),
+        ("prog_dyn", main_prog_dyn),
+        ("sol_1", main_sol_1),
+        ("sol_2", main_sol_2),
+    ];
+    let arg = env::args().nth(1).expect("an argument is required!"); 
+
+    let f = funcs.into_iter().find(|&(n, _)| n == arg).map(|(_, b)| b);
+    
+    match f {
+        None => {
+            println!("Invalid argument! Function {} is not supported by this program", arg);
+            println!("please select one function among: ");
+            for (n, _) in funcs {
+                println!("  - {}", n);
+            }
+        },
+        Some(f) => f(),
+    }
 }
